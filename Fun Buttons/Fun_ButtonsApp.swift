d7ebbb5fc2417c026dea938682b91c2bc50c7597
sakofchit <sakun.acharige@gmail.com>
2023-03-10 21:51:04 -0500

    Initial Commit

---
+++ Fun Buttons/Fun_ButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fun_ButtonsApp.swift
+//  Fun Buttons
+//
+//  Created by Sakun on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fun_ButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
